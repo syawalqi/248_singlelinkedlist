c7388d43c36b45ff8483e48ade55c64643b0e25e
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-25 15:06:43 +0700

    membuat while statemnet

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -33,5 +33,20 @@ void addNode() {
 		return;
 	}
 
+    Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+
 
 
