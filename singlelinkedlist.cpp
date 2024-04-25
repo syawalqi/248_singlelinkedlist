e5659209793f56b229e2e0e27425bca2289b90f3
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-04-25 15:08:00 +0700

    membuat bool search node

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -50,6 +50,15 @@ void addNode() {
 	previous->next = nodeBaru;
 }
 
+bool serachNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
 
 
 
